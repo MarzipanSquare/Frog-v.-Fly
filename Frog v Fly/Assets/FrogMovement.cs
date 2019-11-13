db6e8d63fed51f1cc5f4ed6274618d952c78223a
Cassie Brice <luminescent64@gmail.com>
2019-11-13 00:23:40 -0500

    Add fly random spawning and frog ai

--- Frog v Fly/Assets/FrogMovement.cs
+++ Frog v Fly/Assets/FrogMovement.cs
@@ -4,10 +4,15 @@ using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
 using Quaternion = UnityEngine.Quaternion;
+using Random = UnityEngine.Random;
 using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
 
-enum Direction {Up, Down, Left, Right}
+enum Direction {
+    Up = 0, 
+    Down = 1,
+    Left = 2, 
+    Right = 3}
 
 public class FrogMovement : MonoBehaviour
 {
@@ -18,6 +23,7 @@ public class FrogMovement : MonoBehaviour
 
     private Animator anim; // the frogs animator
     private Rigidbody2D rb; // the frogs rigidbody (for physics and collisions)
+    private Collider2D[] tongueCollider;
 
     // Start is called before the first frame update
     void Start()
@@ -25,42 +31,73 @@ public class FrogMovement : MonoBehaviour
         // get the frog's animator and rigidbody
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        tongueCollider = GetComponentsInChildren<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // every frame check for a key down and move the frog in that direction
-        
-        if (Input.GetKeyDown(KeyCode.UpArrow) || 
-            Input.GetKeyDown(KeyCode.W))
+
+        if (!gameManager.isSim)
         {
-            Move(Direction.Up);
+            #region GameCode
+        
+            // every frame check for a key down and move the frog in that direction
+            if (Input.GetKeyDown(KeyCode.UpArrow) || 
+                Input.GetKeyDown(KeyCode.W))
+            {
+                Move(Direction.Up);
             
-        }
+            }
         
-        if (Input.GetKeyDown(KeyCode.DownArrow) || 
-                   Input.GetKeyDown(KeyCode.S))
-        {
-            Move(Direction.Down);
+            if (Input.GetKeyDown(KeyCode.DownArrow) || 
+                Input.GetKeyDown(KeyCode.S))
+            {
+                Move(Direction.Down);
             
-        }
+            }
         
-        if (Input.GetKeyDown(KeyCode.LeftArrow) || 
-                   Input.GetKeyDown(KeyCode.A))
-        {
-            Move(Direction.Left);
+            if (Input.GetKeyDown(KeyCode.LeftArrow) || 
+                Input.GetKeyDown(KeyCode.A))
+            {
+                Move(Direction.Left);
             
-        }
+            }
         
-        if (Input.GetKeyDown(KeyCode.RightArrow) || 
-                   Input.GetKeyDown(KeyCode.D))
-        {
-            Move(Direction.Right);
+            if (Input.GetKeyDown(KeyCode.RightArrow) || 
+                Input.GetKeyDown(KeyCode.D))
+            {
+                Move(Direction.Right);
+            }
+
+            #endregion
         }
+        else
+        {
+            #region SimulationCode
+
+            if (gameManager.flyActive)
+            {
+                Direction flyDir = GetFlyDirection();
+                
+                // Move(flyDir);
+                // Move((Direction) Random.Range(0, 3));
+                Move(ReverseDirection(flyDir));
+
+            }
+
+            #endregion
+        }
+    
+        
+        #region RaycastForHopping
+
         Vector2 forward = transform.TransformDirection(Vector2.up) * 3;
         RaycastHit2D hit = Physics2D.Raycast(transform.position, forward);
-        Debug.DrawRay(transform.position, forward, Color.white); //uncomment this to see the raycast in the scenes
+        // Debug.DrawRay(transform.position, forward, Color.white); //uncomment this to see the raycast in the scenes
+
+        #endregion
+
     }
 
     void Move(Direction dir)
@@ -86,7 +123,7 @@ public class FrogMovement : MonoBehaviour
         // generate a raycast of length 3 that looks down the frog's nose
         Vector3 forward = transform.TransformDirection(Vector2.up) * 3;
         RaycastHit2D hit = Physics2D.Raycast(transform.position, forward);
-        Debug.DrawRay(transform.position, forward, Color.white); //uncomment this to see the raycast in the scene
+//        Debug.DrawRay(transform.position, forward, Color.white); //uncomment this to see the raycast in the scene
 
         // if the raycast hits a lilypad collider, move the frog to that lilypad
         if (hit.collider.gameObject.CompareTag("LilyPad"))
@@ -98,6 +135,50 @@ public class FrogMovement : MonoBehaviour
         
         // play the frog's hop animation
         anim.Play("frog_hop");
+    }
+
+    Direction GetFlyDirection()
+    {
+        Vector3 directionVector = transform.position - gameManager.fly.transform.position;
+        Debug.Log(directionVector);
+
+        // get absolute values of x and y to find see if the fly is farther horizontally or vertically
+        float xDir = Mathf.Abs(directionVector.x);
+        float yDir = Mathf.Abs(directionVector.y);
+
+        if (xDir >= yDir)
+        {
+            if (directionVector.x < 0)
+            {
+                return Direction.Right;
+            }
+            
+            return Direction.Left;
+        }
+
+        if (directionVector.y < 0)
+        {
+            return Direction.Up;
+        }
+        
+        return Direction.Down;
+    }
+
+    Direction ReverseDirection(Direction dir)
+    {
+        switch (dir)
+        {
+            case Direction.Up:
+                return Direction.Down;
+            case Direction.Down:
+                return Direction.Up;
+            case Direction.Right:
+                return Direction.Left;
+            case Direction.Left:
+                return Direction.Right;
+            default:
+                return (Direction) Random.Range(0, 3);
+        }
     }
     
     // if the fly enters the frog's collider, catch the fly
--- Frog v Fly/Assets/GameManager.cs
+++ Frog v Fly/Assets/GameManager.cs
@@ -1,22 +1,44 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
 
+    // how long lilypads stay underwater
     public int lilypadWaitTime = 2;
+    
+    // is there a fly active on the scene?
     public bool flyActive = true;
     public GameObject flyPrefab; 
     
+    // flag to switch to sim or game
     public bool isSim = false;
+
+    public GameObject fly;
+    private FlyMovement _flyMovement;
+
+    private void Start()
+    {
+        Vector3 position = new Vector3(Random.Range(-9, 9), Random.Range(-5, 5), 0);
+        fly = (GameObject) Instantiate(flyPrefab, position, Quaternion.identity);
+        _flyMovement = fly.GetComponent<FlyMovement>();
+    }
 
     // Update is called once per frame
     void Update()
     {
+        // if there's no fly on the scene, make a new one
         if (flyActive == false)
         {
-            Object.Instantiate(flyPrefab);
+            Vector3 position = new Vector3(Random.Range(-9, 9), Random.Range(-5, 5), 0);
+            fly = (GameObject) Instantiate(flyPrefab, position, Quaternion.identity);
+            _flyMovement = fly.GetComponent<FlyMovement>();
+            
             flyActive = true;
         }
     }
